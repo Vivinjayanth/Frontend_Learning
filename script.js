619d22351283380dd9d8b862c7128984965c9ee7
Vivinjayanth <vivinjayantham@gmail.com>
2026-02-02 10:35:23 +0530

    js learning

---
+++ script.js
@@ -0,0 +1,74 @@
+/*var count = 5;
+
+function show() {
+  console.log(count);
+}
+show();*/
+
+/*let a = 5, b = 10;
+console.log(a + b);*/
+
+/*var age = 20;
+if (age >= 18) {
+  console.log("Eligible");
+} else {
+  console.log("Not Eligible");
+}*/
+
+/*let day = 1;
+
+switch(day) {
+  case 1: console.log("Monday"); break;
+  case 2: console.log("Tuesday"); break;
+  default: console.log("Invalid");
+}*/
+
+/*for(let i=1; i<=5; i++) {
+  console.log(i);
+}*/
+
+/*let i = 1;
+while(i <= 5) {
+  console.log(i);
+  i++;
+}*/
+
+/*function add(a, b) {
+  return a + b;
+}
+
+var a = add(5, 3);
+console.log(a);*/
+
+/*let student = {
+  name: "Rahul",
+  age: 20,
+  course: "JS"
+};
+
+console.log(student.age);*/
+
+/*let fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits[1]);*/
+
+/*let msg = "Hello World";
+var len = msg.length;
+var upper = msg.toUpperCase();
+console.log(msg);
+console.log(len);
+console.log(upper);*/
+
+/*let today = new Date();
+console.log(today);*/
+
+/*console.log(Math.round(4.6));
+console.log(Math.sqrt(36));
+console.log(Math.max(10, 20));*/
+
+/*let n = 10;
+n.toString();
+console.log(typeof n);*/
+
+let isValid = true;
+console.log(typeof isValid);
+
